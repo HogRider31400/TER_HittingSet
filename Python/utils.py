35d14cd7097b9d834182da4fd1d1f1bdc8999284
Leandre GIAMMONA <leandre.giammona@gmail.com>
2025-02-04 11:31:49 +0100

    Update utils.py
    update docu pour utils.py

--- Python/utils.py
+++ Python/utils.py
@@ -1,8 +1,15 @@
 from pathlib import Path
 
 def get_project_root() -> Path:
+    """
+    Donne le chemin du root du projet.
+    """
     return Path(__file__).parent.parent
 
 def get_data_path(filename):
+    """
+    Fonction qui renvoie le chemin vers un fichier .txt contenant des datas d'un hypergraphe.
+    Input : Un nom de fichier
+    Output : Le chemin d'accès de l'utilisateur vers le fichier .txt
+    """
     return str(get_project_root()).replace("\\", "/") + "/C/data/" + filename
-print(get_data_path("example.txt"))
